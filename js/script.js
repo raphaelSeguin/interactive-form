7dcb96b71bf243a733b2f0a931369c14e80f1454
raphaelSeguin <raphaelseguin@gmail.com>
2018-01-30 16:20:11 +0100

    better comments erased useless code enhanced error messages

--- js/script.js
+++ js/script.js
@@ -40,9 +40,12 @@ const inputElements      = document.getElementsByTagName('input');
 const titleHandler = (e) => {
   // if 'other' option is selected in the menu
   if (e.target.value === 'other') {
+    // display the text input elemnt
     titleTextInput.style.display = '';
+    // and set focus on it
     titleTextInput.focus();
   } else {
+    // else hide it
     titleTextInput.style.display = 'none';
   }
 };
@@ -51,11 +54,14 @@ const designHandler = (e) => {
   const choice = e.target.value;
   // Sets disabled attribute of options according to the design selected
   if (choice === 'js puns') {
+    // show menu and label
     colorSelect.style.display = '';
     colorLabel.style.display = '';
+    // set the content of the menu
     colorSelect.innerHTML = `<option value="cornflowerblue">Cornflower Blue</option>
     <option value="darkslategrey">Dark Slate Grey</option>
     <option value="gold">Gold</option>`;
+    // select a valid option
     colorSelect.children[0].selected = true;
   } else if ( choice === 'heart js') {
     colorSelect.style.display = '';
@@ -64,6 +70,7 @@ const designHandler = (e) => {
     <option value="steelblue">Steel Blue</option>
     <option value="dimgrey">Dim Grey</option>`;
   } else {
+    // hide the menu and label if no design is selected
     colorSelect.style.display = 'none';
     colorLabel.style.display = 'none';
     colorSelect.innerHTML = `<option value="Select a design">Please select a design</option>`;
@@ -71,6 +78,7 @@ const designHandler = (e) => {
 };
 // disable label
 const activitiesHandler = (e) => {
+  // here's a handy function to disable an option (uncheck and grey out)
   const disableChoice = (el, bool) => {
     if (bool) {
       el.firstChild.disabled = true;
@@ -94,19 +102,27 @@ const activitiesHandler = (e) => {
     // disable js-frameworks 1
     disableChoice(choices[1], e.target.checked);
   }
+  // erase error, if any, when user selects an activity
+  if (e.target.checked) {
+    notifyError(choices[0], '');
+  }
 }
 // compute total price for activities selected
 const activitiesTotal = () => {
+  // list of prices
   const prices = [200, 100, 100, 100, 100, 100, 100];
   let total = 0;
+  // every check box add its list price if it's checked
   for (let i = 0; i < choices.length; i++) {
     total += choices[i].firstChild.checked ? prices[i] : 0;
   }
   //check for an existing p
   const totalParagraph = activitiesFieldSet.getElementsByTagName('p')[0];
   if (totalParagraph) {
+    // if it exists set its content
     totalParagraph.textContent = `Total: ${total}$`;
   } else {
+    // else create it and append it
     const totalParagraph = document.createElement('p');
     totalParagraph.textContent = `Total: ${total}$`;
     choices[0].parentNode.appendChild(totalParagraph);
@@ -115,7 +131,7 @@ const activitiesTotal = () => {
 // payment options hide / show
 const paymentOptionsDisplay = (e) => {
   const choice = e.target.value;
-  console.log(choice);
+  // every choice make its div show and the others disappear
   if (choice === 'credit card') {
     creditCardDiv.style.display = '';
     paypalDiv.style.display = 'none';
@@ -137,16 +153,20 @@ const paymentOptionsDisplay = (e) => {
 // formValidation
 const formValidation = (e) => {
 
-  //e.preventDefault();
+  // credit card inputs are valid if test return true OR credit card payment is NOT selected
+  const cvvValidity         = validators['cvv'].test(cvvInput)
+                                || !(paymentSelect.value === 'credit card');
+  const zipValidity         = validators['zip'].test(zipInput)
+                                || !(paymentSelect.value === 'credit card');
+  const ccnumValidity       = validators['cc-num'].test(ccnumInput)
+                                || !(paymentSelect.value === 'credit card');
 
-  const cvvValidity         = validators['cvv'].test(cvvInput) || !(paymentSelect.value === 'credit card');
-  const zipValidity         = validators['zip'].test(zipInput) || !(paymentSelect.value === 'credit card');
-  const ccnumValidity       = validators['cc-num'].test(ccnumInput) || !(paymentSelect.value === 'credit card');
-
+  // activities, mail and name validations
   const activitiesValidity  = activitiesValidation();
   const mailValidity        = validators['mail'].test(emailInput);
   const nameValidity        = validators['name'].test(nameInput);
 
+  // form is valid if ALL the fields are valid
   const formValidity        = nameValidity
                               &&
                               mailValidity
@@ -159,39 +179,49 @@ const formValidation = (e) => {
                               &&
                               cvvValidity
                               ;
-
-  if (!cvvValidity) {
-    notifyError(cvvInput, ' ');
+  // if input is invalid, notify a message and set the focus on it
+  // conditionnals are on reverse order so the first error in the page gets the focus
+  // error message is different if the input is empty
+  if ( !cvvValidity ) {
+    let message = cvvInput.value.length === 0
+                  ? 'required'
+                  : 'not valid';
+    notifyError(cvvInput, message);
     cvvInput.focus();
   }
-  if (!zipValidity) {
-    notifyError(zipInput, ' ');
+  if ( !zipValidity ) {
+    let message = zipInput.value.length === 0
+                  ? 'required'
+                  : 'not valid';
+    notifyError(zipInput, message);
     zipInput.focus();
   }
-  if (!ccnumValidity) {
-    notifyError(ccnumInput, ' ');
+  if ( !ccnumValidity ) {
+    let message = ccnumInput.value.length === 0
+                  ? 'required'
+                  : 'not valid';
+    notifyError(ccnumInput, message);
     ccnumInput.focus();
   }
-  if (!activitiesValidity) {
+  if ( !activitiesValidity ) {
     notifyError(choices[0], 'Please, choose an activity');
+    // the page scrolls automatically to the first error in the page
     scrollBy(0, choices[0].parentElement.getBoundingClientRect().top);
   }
-  if (!mailValidity) {
+  if ( !mailValidity ) {
     let message = emailInput.value === ''
-                  ?
-                  'Please, fill in your e-mail'
-                  :
-                  'e-mail not formatted properly'
+                  ? 'Please, fill in your e-mail'
+                  : 'e-mail not formatted properly';
     notifyError(emailInput, message);
     emailInput.focus();
     scrollBy(0, emailInput.parentElement.getBoundingClientRect().top);
   }
-  if (!nameValidity ) {
+  if ( !nameValidity ) {
     notifyError(nameInput, 'Please, fill in your name');
     nameInput.focus();
     scrollBy(0, nameInput.parentElement.getBoundingClientRect().top);
   }
-  // prevent submitting the form if not all validated
+  // prevent submitting the form if not completely validated
   if ( !formValidity ) {
     e.preventDefault();
   }
@@ -208,7 +238,7 @@ const realTimeValidation = (e) => {
 // Validation functions
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 
-// all the validate functions for text inputs, and their error messages
+// all the validate functions for text inputs, and their error messages in an object
 const validators = {
   'name': {
     test: input => /\w/.test(input.value),
@@ -250,14 +280,15 @@ const activitiesValidation = () => {
 }
 
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-// Helper functions
+// Helper function
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 
-// notiiy an error message on the origin element's label
+// notify an error message on the origin element's label
 const notifyError = (origin, message) => {
-  //const destination = getParentElementByTagName(origin, 'FIELDSET').getElementsByTagName('legend')[0];
+  // the message will be displayed in the label element upon the input field
   const destination = origin.previousElementSibling;
   const messageTarget = destination.getElementsByTagName('span')[0];
+  //
   if (message) {
     // add class error
     origin.classList.add('error');
@@ -265,28 +296,17 @@ const notifyError = (origin, message) => {
     // remove class error
     origin.classList.remove('error');
   }
+  // if the span already exist remove it
   if(messageTarget) {
     destination.removeChild(messageTarget);
   }
-  insertMessage(destination, message);
-};
-// insert a message (in a span) after the element
-const insertMessage = (element, message) => {
+  // create a new span
   const errorSpan = document.createElement('span');
   errorSpan.className = 'error-message';
   errorSpan.innerText = message;
-  element.appendChild(errorSpan);
-}
-// find a parent of a certain tag name
-const getParentElementByTagName = (element, tagName) => {
-  while(element.tagName !== tagName) {
-    element = element.parentNode;
-    if (element.tagName === 'BODY') {
-      return null;
-    }
-  }
-  return element;
-}
+  destination.appendChild(errorSpan);
+  //insertMessage(destination, message);
+};
 
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 // Event handling
@@ -298,13 +318,13 @@ activitiesFieldSet.addEventListener('change', activitiesHandler);
 activitiesFieldSet.addEventListener('change', activitiesTotal);
 paymentSelect.addEventListener('change', paymentOptionsDisplay);
 submitButton.addEventListener('click', formValidation, false);
-
+// put a handler on every input element for realtime validation
 Array.from(inputElements).forEach( (element) => {
   element.addEventListener('input', realTimeValidation);
 });
 
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-// Do things
+// Set up the page at load time
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 
 // hide the optionnal job role input text
@@ -312,12 +332,12 @@ titleTextInput.style.display = 'none';
 // hide color selection
 colorSelect.style.display = 'none';
 colorLabel.style.display = 'none';
-// calculate the total price (display Total: 0$ for the moment)
-activitiesTotal();
 // hide payment options by default
 paypalDiv.style.display = 'none';
 bitcoinDiv.style.display = 'none';
 // select credit card by default
 paymentSelect.children[1].selected = 'true';
-// focus on the name field... it seems not to work if not in last with a little delay !!???
+// calculate the total price (display Total: 0$ for the moment)
+activitiesTotal();
+// focus on the name field
 nameInput.focus();
